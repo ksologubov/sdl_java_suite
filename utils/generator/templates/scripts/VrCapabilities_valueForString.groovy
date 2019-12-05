b133e3e14fb70495d0b1a20d94b5bf64d5232575
Kostiantyn Sologubov <ksologubov@luxoft.com>
2019-12-05 16:48:52 +0200

    VrCapabilities override

---
+++ utils/generator/templates/scripts/VrCapabilities_valueForString.groovy
@@ -0,0 +1,22 @@
+    /**
+     * Convert String to VrCapabilities
+     *
+     * @param value String
+     * @return VrCapabilities
+     */
+    public static VrCapabilities valueForString(String value) {
+        if(value == null){
+            return null;
+        }
+
+        if (value.equalsIgnoreCase(TEXT.toString()))
+        {
+        	return TEXT;
+        }
+
+        try{
+            return valueOf(value);
+        }catch(Exception e){
+            return null;
+        }
+    }
