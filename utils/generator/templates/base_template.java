5ffd9e1cf1e52c661676a22d3c1a49d08bf9f085
Kostiantyn Sologubov <ksologubov@luxoft.com>
2020-01-16 12:01:06 +0200

    Copyright year update

--- utils/generator/templates/base_template.java
+++ utils/generator/templates/base_template.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017 - 2019, SmartDeviceLink Consortium, Inc.
+ * Copyright (c) 2020, SmartDeviceLink Consortium, Inc.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
